b8373d6106226ea3f21802216ba4d7455457e921
Amir Ali <amiraliazimloo123@gmail.com>
2023-10-25 22:42:44 +0330

    features completed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Features from './components/templates/index/Features'
 
 
 export default function Home() {
   return (
    <>
-   main Page
+   <Features/>
    </>
   )
 }
