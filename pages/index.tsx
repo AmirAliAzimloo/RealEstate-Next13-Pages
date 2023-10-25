dff579d846d88e5ae5ea8a776b74102811330642
Amir Ali <amiraliazimloo123@gmail.com>
2023-10-25 22:50:32 +0330

    homes completed

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Features from './components/templates/index/Features'
 import Story from './components/templates/index/Story'
+import Homes from './components/templates/index/Homes'
 
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
    <>
    <Features/>
    <Story/>
+   <Homes/>
    </>
   )
 }
