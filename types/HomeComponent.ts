21e0089cd23b5a9bda86099ac2634647b4241ded
Amir Ali <amiraliazimloo123@gmail.com>
2023-10-25 23:08:23 +0330

    home component completed

---
+++ types/HomeComponent.ts
@@ -0,0 +1,10 @@
+export interface HomeComponentProps {
+  id: number;
+  title: string;
+  img: string;
+  roomCount: number;
+  meterage: number;
+  price: number;
+  code: string;
+  desc: string;
+}
