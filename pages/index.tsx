4be4104a46ff38b99388a5b3497a27913c3703d5
Amir Ali <amiraliazimloo123@gmail.com>
2023-10-25 22:44:47 +0330

    story pictures completed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Features from './components/templates/index/Features'
+import StoryPictures from './components/templates/index/StoryPictures'
 
 
 export default function Home() {
   return (
    <>
    <Features/>
+   <StoryPictures/>
    </>
   )
 }
